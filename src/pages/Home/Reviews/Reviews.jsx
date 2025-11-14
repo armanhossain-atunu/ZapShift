6ad49f910e93ade767df2b574f3cdb2cfa7db27a
Programming Hero <programming.hero1@gmail.com>
2025-11-14 10:18:33 -0600

    added review card

--- src/pages/Home/Reviews/Reviews.jsx
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -1,31 +1,42 @@
 import React, { use } from 'react';
-import { EffectCoverflow } from 'swiper/modules';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import Review from './Review';
+import ReviewCard from './ReviewCard';
 
 const Reviews = ({ reviewsPromise }) => {
     const reviews = use(reviewsPromise);
     console.log(reviews);
     return (
-        <Swiper
-            effect={'coverflow'}
-            grabCursor={true}
-            centeredSlides={true}
-            slidesPerView={'auto'}
-            coverflowEffect={{
-                rotate: 50,
-                stretch: 0,
-                depth: 100,
-                modifier: 1,
-                slideShadows: true,
-            }}
-            modules={[EffectCoverflow]}
-            className="mySwiper"
-        >
-            {reviews.map(review => <SwiperSlide key={review.id}>
-                <Review review={review}></Review>
-            </SwiperSlide>)}
-        </Swiper>
+        <div>
+            <div className='text-center'>
+                <h3 className="text-3xl text-center">Review</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non soluta, perspiciatis esse eius praesentium similique asperiores quasi veniam repellat architecto repellendus, deserunt accusantium, debitis ipsa. Expedita possimus facilis voluptas deserunt.</p>
+            </div>
+            <>
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={3}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="mySwiper"
+                >
+                    {
+                        reviews.map(review => <SwiperSlide key={review.id}>
+                            <ReviewCard review={review}></ReviewCard>
+                        </SwiperSlide>)
+                    }
+                </Swiper>
+            </>
+        </div>
     );
 };
 
